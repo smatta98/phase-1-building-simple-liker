47ef7c834f519eefe96531f89927ba63dfc9a171
Shiv Matta <shiv.matta@gmail.com>
2022-07-11 16:03:47 -0700

    commit tree 7f05496a7d4a12c215cab837d74d15f0f3403419

--- main.js
+++ main.js
@@ -1,8 +1,66 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const errorModal = document.querySelector("#modal")
 
 // Your JavaScript code goes here!
+// Add the `.hidden` class to the error modal in the HTML so it does not appear when the page first loads
+document.addEventListener("DOMContentLoaded", () => {
+  errorModal.className = "hidden"
+//   // findLikes()
+//   //clickListener()
+})
+
+  // When a user clicks on an empty heart invoke `mimicServerCall` to simulate making a server request
+//Need an event listener on all the hearts
+
+//function findLikes(){ //this was also works but the second way has a 
+  //reduced amount of event listeners-- Ask Rahul which was is better
+  const heartClicker = document.querySelectorAll('.like-glyph')
+  console.log(heartClicker)
+  // heartClicker.for 
+  for (const button of heartClicker) {
+    button.addEventListener('click', clickListener)
+  }
+  // }((like) => {
+  //   like.addEventListener('click', clickListener)
+
+  //   })
+  
+  // function hideError(){
+  //   
+  // }
+
+  function clickListener(e){
+    const activatedHeart = e.target
+    //document.addEventListener('click', (e) => {
+      //if(e.target.classList.value === 'like-glyph'){
+      // have to invole the server call 
+      console.log(e)
+        mimicServerCall('hello world')
+        .then(() => {
+          // console.log(response)
+          
+          if(activatedHeart.innerHTML === FULL_HEART){
+            e.target.className = ''
+            e.target.innerHTML = EMPTY_HEART
+          } else {
+           e.target.className = 'activated-heart'
+           e.target.innerHTML = FULL_HEART
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+          errorModal.className = ''
+          errorModal.innerText = error
+           // when the promise fails, the .catch executes and removes the error modal which leads to an error message 
+          setTimeout(() => errorModal.className = 'hidden', 3000)
+  
+        })
+      }
+  
+
+
 
 
 
